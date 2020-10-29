920c8a197b4adee0f7e5a9a7a871207508d93cc4
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-29 05:49:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 // Add your code here
 // Add your code here
 function submitData(userName, userEmail){
-    let formData = {
-        name: userName,
-        email: userEmail
-      };
+  let formData = {
+      name: userName,
+      email: userEmail
+    };
 
-      let configObj = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(formData)
-      };
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
 
-      return fetch("http://localhost:3000/users", configObj)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(object) {
-          console.log(object);
-        });
+  return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+    })
+    .then(function(object) {
+      return console.log(object);
+    });
+}
+
+function addToDOM(stuff) {
+  let body = document.querySelector('body')
+  let header = document.createElement('h1')
+  header.innerHTML = stuff
+  body.appendChild(header);
 }
